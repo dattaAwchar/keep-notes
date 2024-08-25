b80e52dae66b49d76d858b19fff6bc10510a3928
dattaAwchar <dattaawchar13@gmail.com>
2024-08-25 13:41:12 +0530

    add data in mongoose

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/"
+
+const connectToMongo = async () =>{
+    await mongoose.connect(mongoURI);
+    console.log('connected to Mongo successfully')
+}
+
+module.exports = connectToMongo;

c8f4f2e40c13dd1e4949220d643734a499916b1a
dattaAwchar <dattaawchar13@gmail.com>
2024-08-25 14:53:45 +0530

    modified validation

--- backend/models/User.js
+++ backend/models/User.js
@@ -24,5 +24,4 @@ const userSchema = new Schema({
 })
 
 const User = mongoose.model('user', userSchema)
-User.createIndexes()
 module.exports = User
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,24 +4,36 @@ const router = express.Router()
 const {body, validationResult} = require('express-validator')
 
 
-// Create a user using: POST "/api/auth/" .Doesn't require Auth
-router.post('/', [
+// Create a user using: POST "/api/auth/createuser" . No login required
+router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({min: 3}),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password contains atleast 5 characters').isLength({min: 5})
-], (req, res) => {
+], async (req, res) => {
+    // If there are errors, return Bad request and the errors
     const error = validationResult(req);
     if(!error.isEmpty()){
         return res.status(400).json({error: error.array()});
     }
-    User.create({
+
+    // check whether the user with this email exists already
+    try{
+    let user = await User.findOne({email: req.body.email})
+    // console.log(User);
+    
+    if(user){
+        return res.status(400).json({error:"sorry, a user with this email already exists"})
+    }
+    user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-    }).then(user => res.json(user))
-    .catch(err=>{console.log(err)
-        res.json({error: "please enter a unique value for email", message:err.message})
     })
+    res.json(user)
+} catch(error){
+    console.log(error.message)
+    res.status(500).send("some error occured")
+}
 })
 
 module.exports = router
